ec6abc6e103746fb053dfefc368e0924ae9556e9
obliviate <146803373+chadgian@users.noreply.github.com>
2026-08-25 15:49:39 +0800

    docs: register in-app system guide route

--- activity_entry.py
+++ activity_entry.py
@@ -1,4 +1,11 @@
+from flask import render_template
+
 from app import app, DB_PATH
 from activity_log import install_activity_logging
 
 install_activity_logging(app, DB_PATH)
+
+
+@app.get("/system-guide")
+def system_guide():
+    return render_template("system_guide.html")
